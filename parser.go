427bde3cdf4bef659b70d1ef405b319664f240a3
Vytor Calixto <vytorcalixto@gmail.com>
2024-10-15 18:35:50 -0300

    fix: check parser

--- parser.go
+++ parser.go
@@ -37,6 +37,94 @@ func getLocalizedText(id string) (string, error) {
 	return localizedText, nil
 }
 
+func uuidParser(matches []string) string {
+	_, name := matches[1], matches[2]
+	if name != "" {
+		return name
+	}
+	return ""
+	// If there's no name, use the last id part
+	// parts := strings.Split(id, ".")
+	// return parts[len(parts)-1]
+}
+
+func localizeParser(matches []string) string {
+	id, name := matches[1], matches[2]
+	localizedText, err := getLocalizedText(id)
+	if err != nil {
+		return err.Error()
+	}
+	if name != "" {
+		return name
+	}
+	return localizedText
+}
+
+func checkParser(matches []string) string {
+	params := matches[1]
+	options := strings.Split(params, "|")
+	var dc, traits, name, basic string
+	name = strings.Title(options[0])
+	for _, option := range options[1:] {
+		if strings.HasPrefix(option, "dc:") {
+			dc = strings.TrimPrefix(option, "dc:")
+		} else if strings.HasPrefix(option, "traits:") {
+			traits = strings.TrimPrefix(option, "traits:")
+		} else if option == "basic" {
+			basic = "Basic "
+		}
+	}
+
+	result := ""
+
+	if dc != "" {
+		result += fmt.Sprintf("DC %s ", dc)
+	}
+	if basic != "" {
+		result += " Basic "
+	}
+	result += fmt.Sprintf(" %s", name)
+
+	if traits != "" {
+		result += fmt.Sprintf(" (%s)", traits)
+	}
+
+	// trim the space at the beginning
+	return strings.Trim(result, " ")
+}
+
+func templateParser(matches []string) string {
+	params := matches[1]
+	name := matches[2]
+	var templateType string
+	var distance string
+	for _, param := range strings.Split(params, "|") {
+		if strings.HasPrefix(param, "distance:") {
+			distance = strings.TrimPrefix(param, "distance:")
+		} else {
+			templateType = param
+		}
+	}
+	if name != "" {
+		return name
+	}
+	return fmt.Sprintf("%s-foot %s", distance, templateType)
+}
+
+func damageParser(matches []string) string {
+	ndmType := matches[1]
+	parts := strings.Split(ndmType, "[")
+	dice := parts[0]
+	damageType := ""
+	if len(parts) > 1 {
+		damageType = strings.Trim(parts[1], "]")
+		if damageType == "untyped" {
+			return dice
+		}
+	}
+	return fmt.Sprintf("%s %s", dice, damageType)
+}
+
 // Main parser
 func parseText(input string) string {
 	uuidRegex := regexp.MustCompile(`@UUID\[(.+?)\](?:{(.+?)})?`)
@@ -48,87 +136,31 @@ func parseText(input string) string {
 	// UUID parser
 	result := uuidRegex.ReplaceAllStringFunc(input, func(match string) string {
 		matches := uuidRegex.FindStringSubmatch(match)
-		id, name := matches[1], matches[2]
-		if name != "" {
-			return name
-		}
-		// If there's no name, use the last id part
-		parts := strings.Split(id, ".")
-		return parts[len(parts)-1]
+		return uuidParser(matches)
 	})
 
 	// Localize parser
 	result = localizeRegex.ReplaceAllStringFunc(result, func(match string) string {
 		matches := localizeRegex.FindStringSubmatch(match)
-		id, name := matches[1], matches[2]
-		localizedText, err := getLocalizedText(id)
-		if err != nil {
-			return err.Error()
-		}
-		if name != "" {
-			return name
-		}
-		return localizedText
+		return localizeParser(matches)
 	})
 
 	// Check parser
 	result = checkRegex.ReplaceAllStringFunc(result, func(match string) string {
 		matches := checkRegex.FindStringSubmatch(match)
-		params := matches[1]
-		options := strings.Split(params, "|")
-		var dc, ability, traits, name, basic string
-		for _, option := range options {
-			if strings.HasPrefix(option, "dc:") {
-				dc = strings.TrimPrefix(option, "dc:")
-			} else if option == "basic" {
-				basic = "Basic "
-			} else if option == "name" {
-				name = "Name "
-			} else {
-				ability = option
-			}
-		}
-		result := fmt.Sprintf("%sDC %s %s", name, dc, basic+ability)
-		if traits != "" {
-			result += fmt.Sprintf(" (%s)", traits)
-		}
-		return result
+		return checkParser(matches)
 	})
 
 	// Template parser
 	result = templateRegex.ReplaceAllStringFunc(result, func(match string) string {
 		matches := templateRegex.FindStringSubmatch(match)
-		params := matches[1]
-		name := matches[2]
-		var templateType string
-		var distance string
-		for _, param := range strings.Split(params, "|") {
-			if strings.HasPrefix(param, "distance:") {
-				distance = strings.TrimPrefix(param, "distance:")
-			} else {
-				templateType = param
-			}
-		}
-		if name != "" {
-			return name
-		}
-		return fmt.Sprintf("%s-foot %s", distance, templateType)
+		return templateParser(matches)
 	})
 
 	// Damage parser
 	result = damageRegex.ReplaceAllStringFunc(result, func(match string) string {
 		matches := damageRegex.FindStringSubmatch(match)
-		ndmType := matches[1]
-		parts := strings.Split(ndmType, "[")
-		dice := parts[0]
-		damageType := ""
-		if len(parts) > 1 {
-			damageType = strings.Trim(parts[1], "]")
-			if damageType == "untyped" {
-				return dice
-			}
-		}
-		return fmt.Sprintf("%s %s", dice, damageType)
+		return damageParser(matches)
 	})
 
 	return result
