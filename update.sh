2e99ddbb7c06b171e03ae628fe586de2b5019675
Vytor Calixto <vytorcalixto@gmail.com>
2025-05-07 11:17:40 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ cp -r ./pf2e/packs/*-bestiary* ./tmp/
 cp -r ./pf2e/packs/npc-gallery ./tmp/
 cp -r ./pf2e/packs/pathfinder-monster-core ./tmp/
 cp -r ./pf2e/packs/pathfinder-dark-archive ./tmp/
+cp -r ./pf2e/packs/pathfinder-npc-core ./tmp/
+cp -r ./pf2e/packs/pathfinder-dark-archive ./tmp/
 
 mkdir -p ./bestiaries
 
